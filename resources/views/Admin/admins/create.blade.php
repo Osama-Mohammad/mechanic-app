f052589a3ee7c91f62cb3979eac1388c0d46d60d
oussama <osama03mohammad@gmail.com>
2025-04-28 15:25:53 +0300

    Touch ups

--- resources/views/Admin/admins/create.blade.php
+++ resources/views/Admin/admins/create.blade.php
@@ -30,34 +30,34 @@
 
                 <form action="{{ route('admin.admins.store') }}" method="POST">
                     @csrf
-                    
+
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
                         <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
                     </div>
-                    
+
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
                         <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
                     </div>
-                    
+
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" id="password" name="password" required>
                     </div>
-                    
+
                     <div class="mb-3">
                         <label for="password_confirmation" class="form-label">Confirm Password</label>
                         <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
                     </div>
-                    
+
                     <div class="d-flex justify-content-end">
                         <button type="submit" class="btn btn-success">
-                            <i class="fas fa-user-plus me-1"></i> Create Admin
+                            <i class="fas fa-user-plus me-1"></i> Create Doctor
                         </button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-</x-layout> 
+</x-layout>
